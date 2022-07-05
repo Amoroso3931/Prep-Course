667d9eef40d2c19b761c27fe762545f6156de2fd
Amoroso3931 <tomiamoroso03@gmail.com>
2022-07-05 10:35:48 -0300

    JS3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,33 +3,33 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+    var array=[1,2,3,4,5,6,7,8];
+    array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  var array=[1,2,3,4,5,6,7,8];
+    array[7];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  var array=[1,2,3,4,5,6,7,8];
+    array;//ahi devuelvo todo el largo
 }
-
-
-function incrementarPorUno(array) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Aumenta cada entero por 1
-  // y devuelve el array
-  // Tu código:
-}
-
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  var array=[1,2,3,4];
+  array.push(5);
+  array;
 }
 
 
@@ -38,6 +38,9 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  var array=[1,2,3,4];
+  array.unshift(5);
+  array;
 }
 
 
@@ -47,6 +50,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var saludo=["hello", " world!"];
+  saludo[0]+saludo[1];
 }
 
 
@@ -54,6 +59,14 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var array=[1,2,3,4,5];
+  elemento=3
+  if(elemento===array[0]||elemento===array[1]||elemento===array[2]||elemento===array[3]||elemento===array[4]||elemento===array[5]){
+    "true"
+  }
+  else{
+    "False"
+  }
 }
 
 
@@ -61,6 +74,9 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var numeros=[1,2,3];
+  var suma=numeros[0]+numeros[1]+numeros[2];
+   suma;
 }
 
 
@@ -88,7 +104,16 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var arreglo=[18,21,1];
+  var n=18;
+    if(arreglo[0]>=n&&arreglo[1]>=n&&arreglo[2]>=n){
+         arreglo[0]
+         arreglo[1]
+         arreglo[2]
+        } 
+    else{
+      "Todos los elementos no son mayor a 18"
+    }
 }
 
 
@@ -97,7 +122,29 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  switch(dia){
+    case '1':
+        "Domingo, es un fin de semana";
+          break;
+    case '2':
+        "Lunes, es un dia laboral";
+          break;
+     case '3':
+        "Martes, es un dia laboral";
+          break;
+     case '4':
+         "Miercoles, es un dia laboral";
+          break;
+      case '5':
+          "Jueves, es un dia laboral";
+          break;
+      case '6':
+          "Viernes, es un dia laboral";
+           break;
+      case '7':
+          "Sabado, es un fin de semana";
+          break;    
+}
 } 
 
 
@@ -105,7 +152,13 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var n=9;
+  if(n>=9){
+    "True"
+  } 
+  else{
+    "False"
+  }
 }
 
 
@@ -113,7 +166,13 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  var arreglo = [1,1,1,1];
+    if(arreglo[0]===arreglo[1]&&arreglo[0]===arreglo[2]&&arreglo[0]===arreglo[3]){
+      "True"
+    }
+    else{
+      "False"
+    }
 } 
 
 
